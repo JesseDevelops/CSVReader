10c6e8e8f8125d6ddc148807bfef1abbb4f07624
JesseDevelops <jessegajda@hotmail.com>
2021-03-30 01:15:39 -0400

    Finished application- Now understand how CSV enters as data.

--- CSVReader/Program.cs
+++ CSVReader/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSVReader
 {
@@ -6,7 +7,39 @@ namespace CSVReader
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Store all contents from csv file into string array
+            string[] rawCSVData = System.IO.File.ReadAllLines(@"C:\Users\Jesse\Desktop\username.csv");
+
+            var userName = new List<string>();
+            var Id = new List<int>();
+            var firstName = new List<string>();
+            var lastName = new List<string>();
+
+            //split each row into column data
+            for (int i = 1; i < rawCSVData.Length; i++)
+            {
+                string[] rowData = rawCSVData[i].Split(',');
+                
+                userName.Add(rowData[0]);
+                int identifier = Convert.ToInt32(rowData[1]);
+                Id.Add(identifier);
+                firstName.Add(rowData[2]);
+                lastName.Add(rowData[3]);
+
+            }
+           
+            //output through another loop using a list of usernames, can do it through anything really.
+            for (int j = 0; j < userName.Count; j++)
+            {
+
+                Console.WriteLine("User number: " + j);
+                Console.WriteLine("Username: " + userName[j]);
+                Console.WriteLine("Id: " + Id[j]);
+                Console.WriteLine("First Name: " + firstName[j]);
+                Console.WriteLine("Last Name: " + lastName[j]);
+                Console.WriteLine(" ");
+            }
+
         }
     }
 }
