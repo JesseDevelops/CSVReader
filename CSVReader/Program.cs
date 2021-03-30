08aad659514ed68b1e049f82a60cab1b7f9f94e0
JesseDevelops <jessegajda@hotmail.com>
2021-03-30 00:46:49 -0400

    Add project files.

---
+++ CSVReader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSVReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
